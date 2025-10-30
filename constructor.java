1739ced191d9ffa41d3a21f9cf188bb27ca29c7e
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2025-10-30 19:28:46 +0530

    default constructor

--- constructor.java
+++ constructor.java
@@ -1,3 +1,27 @@
 /*
 constructor start
+ - constructor is a function with the same name as classname but without return type.
+ -  constructor get executed when we create object of class
+ - types
+	* Default constructor. 
+	* Parameterized constructor. 
+	* Overloaded constructor. 
+	* this() constructor. 
+
 */
+import java.util.*;
+public class constructor{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of variable");
+		int n = sc.nextInt();
+		square sq = new square();// example of default constructor
+		sq.calSquare(n);
+	}
+}
+class square{
+	void calSquare(int n){
+		System.out.println("Square is : "+ n*n);
+	}
+}
